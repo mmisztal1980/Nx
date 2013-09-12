0ae7b61e81b5112f542e4ebe175ca2e778ad2d9c
mmisztal1980 <mmisztal1980@gmail.com>
2013-09-12 17:39:36 +0200

    Nx.Logging initial commit

---
+++ Nx.Logging/Nx.Logging.Net40/NLogLogger.cs
@@ -0,0 +1,64 @@
+﻿using NLog;
+using System;
+
+namespace Nx.Logging
+{
+    public class NLogLogger : ILogger
+    {
+        private Logger Logger;
+
+        public void Debug(string message, params object[] args)
+        {
+            Logger.Debug(message, args);
+        }
+
+        public void Error(string message, params object[] args)
+        {
+            Logger.Error(message, args);
+        }
+
+        public void Fatal(string message, params object[] args)
+        {
+            Logger.Fatal(message, args);
+        }
+
+        public void Info(string message, params object[] args)
+        {
+            Logger.Info(message, args);
+        }
+
+        public void Warning(string message, params object[] args)
+        {
+            Logger.Warn(message, args);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Logger = null;
+            }
+        }
+
+        public NLogLogger()
+        {
+            Logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public NLogLogger(string loggerName)
+        {
+            Logger = LogManager.GetLogger(loggerName);
+        }
+
+        ~NLogLogger()
+        {
+            Dispose(false);
+        }
+    }
+}
