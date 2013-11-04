60e7fd8a60213f5512e175ad15938048fcaffa10
mmisztal <mmisztal1980@gmail.com>
2013-11-04 23:31:35 +0100

    tests added

---
+++ Nx.Mongo/Nx.Mongo.IntegrationTests/Bootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using Ninject;
+using Nx.Bootstrappers;
+using Nx.Kernel;
+using System.Configuration;
+
+namespace Nx.Mongo.IntegrationTests
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        protected override void ConfigureContainer()
+        {
+            Kernel.RegisterTypeIfMissing<IMongoContextFactory, MongoContextFactory>(false);
+            Kernel.RegisterTypeIfMissing<IMongoEntityRepository, MongoEntityRepository>(false);
+        }
+
+        protected override Ninject.IKernel CreateContainer()
+        {
+            return new StandardKernel();
+        }
+
+        private void RegisterConfigurationSection<T>(string sectionName)
+            where T : ConfigurationSection
+        {
+            var section = GetConfigurationSection<T>(sectionName);
+            if (section != null)
+            {
+                Kernel.RegisterInstance<T>(section);
+            }
+        }
+
+        private T GetConfigurationSection<T>(string sectionName)
+            where T : ConfigurationSection
+        {
+            return (T)ConfigurationManager.GetSection(sectionName);
+        }
+    }
+}
