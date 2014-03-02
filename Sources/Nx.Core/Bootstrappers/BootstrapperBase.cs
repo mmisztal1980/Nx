b3131f2236d303d0f2f0d5c85f0b03c6db749a49
Maciek Misztal <mmisztal1980@gmail.com>
2014-03-02 12:27:18 +0100

    missing files added

---
+++ Sources/Nx.Core/Bootstrappers/BootstrapperBase.cs
@@ -0,0 +1,146 @@
+﻿using Ninject;
+using Ninject.Infrastructure;
+using Ninject.Modules;
+using Nx.Kernel;
+using Nx.Logging;
+using System;
+
+namespace Nx.Bootstrappers
+{
+    /// <summary>
+    /// The Bootstrapper. Executes the initialization sequence for the application. 
+    /// </summary>
+    public abstract class BootstrapperBase : IHaveKernel, IBootstrapper, IDisposable
+    {
+        protected readonly INinjectSettings KernelSettings = null;
+        protected readonly IModuleLoadingStrategy ModuleLoadingStrategy = null;
+        protected bool UseDefaultConfiguration = true;
+        protected const string LoggerName = "SYSTEM";
+
+        public IKernel Kernel { get; protected set; }
+
+        public BootstrapperBase(params INinjectModule[] modules)
+        {
+            ModuleLoadingStrategy = new HardcodedLoadingStrategy(modules);
+
+            Kernel = CreateContainer();
+            PreConfigureContainer();
+            Kernel
+                .Bind<ILogFactory>()
+                .To<LogFactory>()
+                .InSingletonScope();
+
+            ConfigureLogging();
+        }
+
+        public BootstrapperBase(INinjectSettings settings, params INinjectModule[] modules)
+            : this(modules)
+        {
+            KernelSettings = settings;
+        }
+
+        #region IDisposable Members
+        ~BootstrapperBase()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                OnDisposing();
+            }
+        }
+
+        public virtual void OnDisposing()
+        {
+        }
+        #endregion
+
+        public IKernel Run()
+        {
+            using (ILogger log = GetLogger(LoggerName))
+            {
+                log.Info("Starting the bootstrapper sequence...");
+
+                log.Info("Configuring the container...");
+                this.ConfigureContainer();
+
+                log.Info("Loading modules...");
+                this.LoadModules();
+
+                return this.Kernel;
+            }
+        }
+
+        public IBootstrapper ExtendBy(Action<IKernel> action)
+        {
+            using (ILogger log = GetLogger(LoggerName))
+            {
+                action(Kernel);
+                log.Info("Applied custom action extension");
+                return this;
+            }
+        }
+
+        public IBootstrapper ExtendBy(IBootstrapperExtension extension)
+        {
+            using (ILogger log = GetLogger(LoggerName))
+            {
+                extension.Extend(this.Kernel);
+                log.Info("Applied Bootstrapper Extension : {0}", extension.Name);
+                return this;
+            }
+        }
+
+        public IBootstrapper ExtendBy<TBootstrapperExtension>()
+            where TBootstrapperExtension : IBootstrapperExtension, new()
+        {
+            return ExtendBy(new TBootstrapperExtension());
+        }
+
+        protected void LoadModules()
+        {
+            this.ModuleLoadingStrategy.LoadModules(Kernel);
+        }
+
+        /// <summary>
+        /// Creates the <see cref="IKernel"/> that will be used as the default container.
+        /// </summary>
+        /// <returns>A new instance of <see cref="IKernel"/>.</returns>
+        protected abstract IKernel CreateContainer();
+
+        /// <summary>
+        /// Ensures the IKernel is registered. Ensures the ServiceLocator is initialized
+        /// </summary>
+        protected virtual void PreConfigureContainer()
+        {
+            if (!Kernel.IsRegistered<IKernel>())
+            {
+                this.Kernel.Bind<IKernel>().ToConstant(this.Kernel).InSingletonScope();
+            }
+        }
+
+        /// <summary>
+        /// Configures the logging via the Nx.Logging.ILogger interface
+        /// </summary>
+        protected virtual void ConfigureLogging()
+        {
+            this.Kernel.Bind<ILogger>().To<NullLogger>().InTransientScope();
+        }
+
+        protected abstract void ConfigureContainer();
+
+        private ILogger GetLogger(string loggerName)
+        {
+            return Kernel.Get<ILogFactory>().CreateLogger(loggerName);
+        }
+    }
+}
---
+++ Sources/Nx.Core/Bootstrappers/HardcodedLoadingStrategy.cs
@@ -0,0 +1,27 @@
+﻿using Ninject;
+using Ninject.Modules;
+
+namespace Nx.Bootstrappers
+{
+    /// <summary>
+    /// This strategy loads the modules passed as an array into the Kernel. 
+    /// Useful in scenarios where the platform doesn't allow to load assemblies, such as WP7+
+    /// </summary>
+    public sealed class HardcodedLoadingStrategy : IModuleLoadingStrategy
+    {
+        public HardcodedLoadingStrategy(params INinjectModule[] modules)
+        {
+            this.modules = modules;
+        }
+
+        public void LoadModules(IKernel kernel)
+        {
+            if (modules != null)
+            {
+                kernel.Load(modules);
+            }
+        }
+
+        private readonly INinjectModule[] modules;
+    }
+}
