acb9969c2b51f390dcd71c3f8a9ed7531b163d16
Maciek Misztal <mmisztal1980@gmail.com>
2014-03-02 12:22:12 +0100

    refactoring

---
+++ Sources/Nx.Core/Kernel/ComponentAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Nx.Kernel
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public sealed class ComponentAttribute : Attribute
+    {
+        public ComponentAttribute(ComponentLifestyle componentLifestyle)
+        {
+            ComponentLifestyle = componentLifestyle;
+        }
+
+        public ComponentLifestyle ComponentLifestyle { get; private set; }
+    }
+}
---
+++ Sources/Nx.Domain/Commands/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nx.Domain.Commands
+{
+    public interface ICommandHandler<T>
+        where T : class, ICommand
+    {
+        void HandleCommand(T command);
+    }
+}
