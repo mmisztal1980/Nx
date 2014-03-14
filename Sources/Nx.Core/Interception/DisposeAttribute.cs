625af2d1018c1f7ebdd2c40656273906c8fe1eda
Maciek Misztal <mmisztal1980@gmail.com>
2014-03-14 12:58:33 +0100

    unused tests removed

--- Sources/Nx.Core/Interception/DisposeAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Nx.Interception
-{
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Event | AttributeTargets.GenericParameter | AttributeTargets.Interface, AllowMultiple = false)]
-    public class DisposeAttribute : Attribute
-    {
-    }
-}
