074f5b4c2337e4d5bc08a17ead339e9778e4079a
Maciek Misztal <mmisztal1980@gmail.com>
2014-06-12 23:25:40 +0200

    build fixed

---
+++ Sources/Nx.Domain/Commands/Command.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Nx.Domain.Commands
+{
+    /// <summary>
+    /// An abstract Command DTO, implementing ICommand.
+    /// Always has an initialized Id.
+    /// Serializable
+    /// </summary>
+    [Serializable]
+    public abstract class Command : ICommand
+    {
+        /// <summary>
+        /// Creates a new Command instance and sets the Id value to a new Guid.
+        /// </summary>
+        protected Command()
+            : this(Guid.NewGuid())
+        {
+        }
+
+        /// <summary>
+        /// Creates a new Command instance and sets the Id value to passed value.
+        /// </summary>
+        /// <param name="id"></param>
+        protected Command(Guid id)
+        {
+            Condition.Require<InvalidOperationException>(id.Equals(Guid.Empty), "You cannot use an empty GUID for a command Id");
+            Id = id;
+        }
+
+        /// <summary>
+        /// The Command's Id
+        /// </summary>
+        public Guid Id { get; private set; }
+    }
+}
---
+++ Sources/Nx.Domain/Commands/CommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Nx.Domain.Commands
+{
+    public abstract class CommandHandler<T> : ICommandHandler<T>
+        where T : class, ICommand
+    {
+        private readonly IDisposable _subscription;
+
+        protected CommandHandler(MassTransit.IServiceBus bus, string queueName)
+        {
+        }
+
+        public abstract void HandleCommand(T command);
+    }
+}
